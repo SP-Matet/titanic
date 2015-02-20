1d6ee4d0175ad105a9ddbf839d094a865fea0c38
Sébastien Dubois <sdubois.sebastien@gmail.com>
2015-02-20 12:50:14 +0100

    Analysis feature selection and give a first guess for the 5 best features

--- titanic-master/main.py
+++ titanic-master/main.py
@@ -8,6 +8,7 @@ from data_loading import *
 from preprocessing import *
 from numpy import *
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier 
 
 data,X,Y = get_data('train.csv')
@@ -15,53 +16,40 @@ print data.head(1)
 
 # split data in training and testing sets
 print data.shape[0]
+training_size = 70*data.shape[0]/100 # 70% for training set
+n_times =20
+n_estimators = 20
+
+# show analysis on feature selection / ordering
+show_text_idx(n_times,training_size,n_estimators,X,Y)
+
+# split data into training and test sets
 idx = range(0,data.shape[0])
 random.shuffle(idx)
-training_size = 70*data.shape[0]/100 # 70% for training set
-
 training_data = X[idx[:training_size],:]
 training_label = Y[idx[:training_size]]
 test_data = X[idx[training_size:],:]
 test_label = Y[idx[training_size:]]
+
+my_idx = [7,4,0,1,6] # selected features
+training_data = training_data[:,my_idx]
+test_data = test_data[:,my_idx]
+
 print training_data.shape
 print test_data.shape
 
-show_ordered_feat(training_data,training_label)
-feats = select_feat(4,training_data,training_label)
-
-X = training_data[:,feats]
-print X.shape
-print training_data.shape
-
 # Create the random forest object 
-forest1 = RandomForestClassifier(n_estimators = 3)
+forest = RandomForestClassifier(n_estimators =25)
 # Fit the training data to the Survived labels and create the decision trees
-forest1 = forest1.fit(training_data,training_label)
+forest = forest.fit(training_data,training_label)
 # Take the same decision trees and run it on the test data
-prediction1 = forest1.predict(test_data)
-train_prediction1 = forest1.predict(training_data)
-
-######### Test with limited number of features
-# Create the random forest object 
-forest2 = RandomForestClassifier(n_estimators = 30)
-# Fit the training data to the Survived labels and create the decision trees
-forest2 = forest2.fit(X,training_label)
-# Take the same decision trees and run it on the test data
-prediction2 = forest2.predict(test_data[:,feats])
-train_prediction2 = forest2.predict(X)
-
-
-print'\n** with all features **'
-print 'Results on training values :'
-show_results(training_label,train_prediction1)
+prediction = forest.predict(test_data)
+train_pred = forest.predict(training_data)
 
 print '\nResults on test values :'
-show_results(test_label,prediction1)
-
-
-print'\n** with selected numbers of features **'
+show_results(test_label,prediction)
 print 'Results on training values :'
-show_results(training_label,train_prediction2)
+show_results(training_label,train_pred)
 
-print '\nResults on test values :'
-show_results(test_label,prediction2)
+
+
